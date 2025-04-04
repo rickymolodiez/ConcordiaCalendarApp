4c4070b2fe4a9114677d4d38c95ea8e5d8f3b34d
rickymolodiez <rickymoloqc@gmail.com>
2025-04-04 09:11:06 -0400

    fix bug
    no return value in extract_catgegory fun, fixed it to update the event category tag

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,7 @@ import time
 import schedule
 
 # Simple main file to run the functions
-if __name__ == "__main__":
-    
+if __name__ == "__main__": 
     schedule.every(2).hours.do(store)
     while True:
         schedule.run_pending()
